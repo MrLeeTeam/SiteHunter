f036fb928eef9923b738b14c9f2a555b2737fb37
redd0g <eceipeno@gamil.com>
2013-04-16 10:57:55 +0900

    fix normalizer adding post_no

--- normalizer.py
+++ normalizer.py
@@ -18,10 +18,10 @@ normal_form = {realm_Tistory: "http://%(id)s.tistory.com/%(post_no)s",
                realm_Naver: "http://blog.naver.com/%(id)s/%(post_no)s",
                realm_Egloos: "http://%(id)s.egloos.com/%(post_no)s"}
 
-normal_base_form = {realm_Tistory: "%(id)s.tistory.com/",
+normal_base_form = {realm_Tistory: "%(id)s.tistory.com",
                realm_Daum: "blog.daum.net/%(id)s",
                realm_Naver: "blog.naver.com/%(id)s",
-               realm_Egloos: "%(id)s.egloos.com/"}
+               realm_Egloos: "%(id)s.egloos.com"}
 
 
 def get_normal_form(realm):
@@ -46,18 +46,21 @@ def normalize(url):
     if  "tistory.com" in url:
         realm = realm_Tistory
         author_id = re.match(".*?([a-zA-Z0-9_-]+).tistory.com", url).group(1)
+        post_no = re.match(".*?.tistory.com/([a-zA-Z0-9_-]+)", url).group(1)
 
     elif "blog.daum.net" in url:
         realm = realm_Daum
-        author_id = re.match(".*?blog.daum.net/([a-zA-Z0-9_-]+)/",url).group(1)
+        author_id = re.match(".*?blog.daum.net/([a-zA-Z0-9_-]+)/", url).group(1)
+        post_no = re.match(".*?blog.daum.net/[a-zA-Z0-9_-]+/([a-zA-Z0-9_-]+)", url).group(1)
 
     elif "naver.com" in url:
         realm = realm_Naver
-        author_id = re.match(".*?blog.naver.com/([a-zA-Z0-9_-]+)/",url).group(1)
+        author_id = re.match(".*?blog.naver.com/([a-zA-Z0-9_-]+)/", url).group(1)
+        post_no = re.match(".*?blog.naver.com/[a-zA-Z0-9_-]+/([a-zA-Z0-9_-]+)", url).group(1)
 
     elif "egloos.com" in url:
         realm = realm_Egloos
-        author_id = re.match(".*?([a-zA-Z0-9_-]+).egloos.com",url).group(1)
+        author_id = re.match(".*?.egloos.com([a-zA-Z0-9_-]+)", url).group(1)
 
 
     result = dict()
@@ -70,3 +73,4 @@ def normalize(url):
     result["mobile_url"] = get_normal_mobile_form(realm) % result
 
     return result
+
